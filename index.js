beaf25e5e3b4816afceb7ec66fc8b5e325b8a4a9
Tuukahi <46800816+Tuukkahi@users.noreply.github.com>
2021-01-02 16:00:27 +0200

    to heroku

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const cors = require('cors')
+
+app.use(cors())
+app.use(express.json())
+morgan.token('body', req => JSON.stringify(req.body)) 
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
+
+let persons = [
+    {
+        name: "Arto Hellas",
+        number: "040-123456",
+        id: 1
+    },
+    {
+        name: "Ada Lovelace",
+        number: "39-44-5323523",
+        id: 2
+    },
+    {
+        name: "Dan Abramov",
+        number: "12-43-234345",
+        id: 3
+    },
+    {
+        name: "Mary Poppendieck",
+        number: "39-23-6423122",
+        id: 4
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+})
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    person = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: 'name missing'
+        })
+    }
+    if (!body.number) {
+        return response.status(400).json({
+            error: 'number missing'
+        })
+    }
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.floor(Math.random() * Number.MAX_SAFE_INTEGER),
+    }
+
+    if(persons.some(person => person.name === body.name)) {
+        return response.status(400).json({
+            error: 'namemust be unique'
+        })
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
